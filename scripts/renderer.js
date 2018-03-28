c1124fac26337f8a4ddbcc3653cabb549802718f
emerytan <emery.t.anderson@gmail.com>
2018-03-28 16:59:45 -0400

    GUI cleanup

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,3 +1,4 @@
+const { dialog } = require('electron').remote
 const { spawn, exec } = require('child_process')
 const ipc = require('electron').ipcRenderer
 const { StringDecoder } = require('string_decoder')
@@ -6,30 +7,9 @@ const $ = jQuery = require('jquery')
 
 
 document.getElementById('buttons').addEventListener('click', (element, event) => {
-  console.log(element.target.id);
+  
   let clickedButton = element.target.id
-  ipc.send('asynchronous-message', clickedButton)
+
 })
 
 $('#debugMessages').text('jquery works')
-
-ipc.on('asynchronous-reply', function (event, arg) {
-  let pinToBottom = document.getElementById(arg.bashOutput)
-  document.getElementById(arg.bashOutput).textContent += decoder.write(arg.data)
-
-})
-
-
-const script = spawn('./bin/clients/1619/2017/ltocontrol/bashScripts/statTape1.sh')
-
-script.stderr.on('data', (data) => {
-  document.getElementById('debugMessages').innerText += decoder.write(data)
-})
-
-script.stdout.on('data', (data) => {
-  document.getElementById('bashOutput1').textContent += decoder.write(data)
-})
-
-script.on('error', (error) => {
-  console.log(error);
-})
